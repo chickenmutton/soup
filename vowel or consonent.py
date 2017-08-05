77f26ca99c87333ed58f0db3aff8a4dff4d509e6
chickenmutton <30741911+chickenmutton@users.noreply.github.com>
2017-08-05 11:06:57 +0530

    Update vowel or consonent.py

--- vowel or consonent.py
+++ vowel or consonent.py
@@ -1,8 +1,8 @@
- l = input("Input a letter of the alphabet: ")
+  l = input("Input a letter of the alphabet: ")
 
- if l in ('a', 'e', 'i', 'o', 'u'):
+  if l in ('a', 'e', 'i', 'o', 'u'):
 	print("%s is a vowel." % l)
- elif l == 'y':
+   l == 'y':
 	print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
  else:
 	print("%s is a consonant." % l)
